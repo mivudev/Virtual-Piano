d6e819e10010a8c32acb7bda382cbd665d1b47a3
mivudev <mivudev@gmail.com>
2020-11-23 18:21:30 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,79 @@
+document.addEventListener("keydown", function (event) {
+
+    if (event.code === "KeyA") {
+        console.log("The 'A' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/A.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyS") {
+        console.log("The 'S' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/S.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyD") {
+        console.log("The 'D' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/D.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyF") {
+        console.log("The 'F' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/F.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyG") {
+        console.log("The 'G' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/G.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyH") {
+        console.log("The 'H' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/H.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyJ") {
+        console.log("The 'J' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "white_keys/J.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyW") {
+        console.log("The 'W' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/W.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyE") {
+        console.log("The 'E' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/E.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyT") {
+        console.log("The 'T' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/T.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyY") {
+        console.log("The 'Y' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/Y.mp3";
+        audio.play();
+    }
+    else if (event.code === "KeyU") {
+        console.log("The 'U' key is pressed.");
+        let audio = document.createElement("AUDIO");
+        audio.src = "black_keys/U.mp3";
+        audio.play();
+    }
+    else {
+        console.log("The key does not exist.");
+        alert("The key does not exist.")
+    }
+});
